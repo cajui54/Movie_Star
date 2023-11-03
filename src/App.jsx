b9eb42436ae66c97d8cca1cb8a0ba3e6d4abb3fd
Jackson Silva <cajui54@gmail.com>
2023-11-03 10:26:30 -0300

    project movies start with query api of movies

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Link, Outlet } from "react-router-dom"
+import Navbar from "./components/Navbar"
+
+function App() {
+  
+  return (
+    <div>
+      <Navbar/>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default App
